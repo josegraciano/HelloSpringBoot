798d9c41727eacf7aa14e467e1a55e5999d810f6
José Ricardo Graciano <jricardo.graciano@outlook.com>
2019-05-30 16:33:29 -0300

    Módulo 6
    Aula 2 - 15m

---
+++ src/main/java/br/com/treinaweb/springboot/controllers/InstituicoesController.java
@@ -0,0 +1,36 @@
+package br.com.treinaweb.springboot.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.treinaweb.springboot.entidades.Instituicao;
+import br.com.treinaweb.springboot.repositorios.RepositorioInstituicao;
+
+@Controller
+@RequestMapping("/instituicoes")
+public class InstituicoesController {
+
+	@Autowired
+	private RepositorioInstituicao repositorioInstituicao;
+
+	@GetMapping("/index")
+	public ModelAndView index() {
+		ModelAndView resultado = new ModelAndView("instituicao/index");
+		List<Instituicao> instituicoes = repositorioInstituicao.findAll();
+		resultado.addObject("instituicoes", instituicoes);
+		return resultado;
+	}
+
+	@GetMapping("/inserir")
+	public ModelAndView inserir() {
+		ModelAndView resultado = new ModelAndView("instituicao/inserir");
+
+		resultado.addObject("instituicao", new Instituicao());
+		return resultado;
+	}
+}
